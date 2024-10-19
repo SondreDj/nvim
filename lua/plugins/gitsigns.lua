ac97493700872b7ed93c4b6d434591af956bb5d8
SondreDj <141500221+SondreDj@users.noreply.github.com>
2024-10-19 18:23:22 +0200

    Git blame + fugitive

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,6 +1,9 @@
 return {
   "lewis6991/gitsigns.nvim",
   config = function ()
-    require("gitsigns").setup()
+    require("gitsigns").setup({
+      current_line_blame = true
+    })
+    vim.keymap.set("n", "<leader>gb", require("gitsigns").toggle_current_line_blame, {desc="Toggle current line blame"})
   end
 }
