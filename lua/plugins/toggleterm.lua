71a81f8d2698ffb67b16cf0dc75b347ab57da422
SondreDj <141500221+SondreDj@users.noreply.github.com>
2024-11-01 10:39:51 +0100

    Harpoon
    Harpoon + lazygit

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,7 +2,6 @@ return {
   'akinsho/toggleterm.nvim',
   config = function ()
     require("toggleterm").setup()
-    vim.keymap.set("n", "<leader>lg", function () vim.cmd("TermExec cmd=~/lazygit/lazygit;exit direction=float") end, {desc="Lazygit"})
     vim.keymap.set("n", "<leader>t", function () vim.cmd("ToggleTerm") end, {desc="ToggleTerm"})
   end
 }
