d0f081a0bc56e55a99c063badee914e5286d95ae
SondreDj <141500221+SondreDj@users.noreply.github.com>
2024-11-21 10:38:12 +0100

    Harpoon lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,9 +1,12 @@
 return {
   'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  dependencies = { 'nvim-tree/nvim-web-devicons', 'letieu/harpoon-lualine' },
   opts = {
     options = {
       theme = "catppuccin"
-    }
+    },
+    sections = {
+      lualine_c = { 'filename', 'harpoon2' },
+    },
   }
 }
